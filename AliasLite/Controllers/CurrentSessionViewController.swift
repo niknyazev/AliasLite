758ea522e1145e2fd0c8cf02785c0a8fce2c801d
niknyazev <n.knyazev.dev@gmail.com>
2022-08-11 13:12:06 +0300

    Add buttons for current session

--- AliasLite/Controllers/CurrentSessionViewController.swift
+++ AliasLite/Controllers/CurrentSessionViewController.swift
@@ -9,24 +9,69 @@ import UIKit
 
 class CurrentSessionViewController: UIViewController {
 
-
+    private lazy var guessWordButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = UIColor(red: 21/255, green: 101/255, blue: 192/255, alpha: 1)
+        button.setTitle("New game", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 4
+//        button.addTarget(self, action: #selector(startNewGamePressed), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var dropWordButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = UIColor(red: 21/255, green: 101/255, blue: 192/255, alpha: 1)
+        button.setTitle("New game", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 4
+//        button.addTarget(self, action: #selector(startNewGamePressed), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var pauseButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = UIColor(red: 21/255, green: 101/255, blue: 192/255, alpha: 1)
+        button.setTitle("New game", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 4
+//        button.addTarget(self, action: #selector(startNewGamePressed), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var finishButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = UIColor(red: 21/255, green: 101/255, blue: 192/255, alpha: 1)
+        button.setTitle("New game", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 4
+//        button.addTarget(self, action: #selector(startNewGamePressed), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var currentWordLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Any word"
+        label.font = .systemFont(ofSize: 20)
+        label.textColor = .black
+        return label
+    }()
+    
+    private lazy var playerName: UILabel = {
+        let label = UILabel()
+        label.text = "User name"
+        label.font = .systemFont(ofSize: 30)
+        label.textColor = .black
+        return label
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .green
-
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
