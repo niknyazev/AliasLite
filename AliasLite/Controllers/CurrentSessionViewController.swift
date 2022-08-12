0c7bb00009078d078533354293e403791afaf438
niknyazev <n.knyazev.dev@gmail.com>
2022-08-12 13:28:02 +0300

    Add timer

--- AliasLite/Controllers/CurrentSessionViewController.swift
+++ AliasLite/Controllers/CurrentSessionViewController.swift
@@ -6,24 +6,46 @@
 //
 
 import UIKit
+import CoreData
 
 class CurrentSessionViewController: UIViewController {
 
+    var timer = Timer()
+    var time = 5 {
+        didSet {
+            timerLabel.text = "\(time)"
+        }
+    }
+    
+    private lazy var wordsManagingButtonsStack: UIStackView = {
+        let result = UIStackView(arrangedSubviews: [guessWordButton, dropWordButton])
+        result.distribution = .fillEqually
+        result.spacing = 10
+        return result
+    }()
+    
+    private lazy var sessionControlButtonsStack: UIStackView = {
+        let result = UIStackView(arrangedSubviews: [startPauseButton, finishButton])
+        result.distribution = .fillEqually
+        result.spacing = 10
+        return result
+    }()
+    
     private lazy var guessWordButton: UIButton = {
         let button = UIButton()
         button.backgroundColor = UIColor(red: 21/255, green: 101/255, blue: 192/255, alpha: 1)
-        button.setTitle("New game", for: .normal)
+        button.setTitle("Guess", for: .normal)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
         button.setTitleColor(.white, for: .normal)
         button.layer.cornerRadius = 4
-//        button.addTarget(self, action: #selector(startNewGamePressed), for: .touchUpInside)
+        button.addTarget(self, action: #selector(startPausePressed), for: .touchUpInside)
         return button
     }()
     
     private lazy var dropWordButton: UIButton = {
         let button = UIButton()
         button.backgroundColor = UIColor(red: 21/255, green: 101/255, blue: 192/255, alpha: 1)
-        button.setTitle("New game", for: .normal)
+        button.setTitle("Drop", for: .normal)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
         button.setTitleColor(.white, for: .normal)
         button.layer.cornerRadius = 4
@@ -31,17 +53,17 @@ class CurrentSessionViewController: UIViewController {
         return button
     }()
     
-    private lazy var pauseButton: UIButton = {
+    private lazy var startPauseButton: UIButton = {
         let button = UIButton()
         button.backgroundColor = UIColor(red: 21/255, green: 101/255, blue: 192/255, alpha: 1)
         button.setTitle("New game", for: .normal)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
         button.setTitleColor(.white, for: .normal)
         button.layer.cornerRadius = 4
-//        button.addTarget(self, action: #selector(startNewGamePressed), for: .touchUpInside)
+        button.addTarget(self, action: #selector(startPausePressed), for: .touchUpInside)
         return button
     }()
-    
+        
     private lazy var finishButton: UIButton = {
         let button = UIButton()
         button.backgroundColor = UIColor(red: 21/255, green: 101/255, blue: 192/255, alpha: 1)
@@ -61,7 +83,7 @@ class CurrentSessionViewController: UIViewController {
         return label
     }()
     
-    private lazy var playerName: UILabel = {
+    private lazy var playerNameLabel: UILabel = {
         let label = UILabel()
         label.text = "User name"
         label.font = .systemFont(ofSize: 30)
@@ -69,9 +91,71 @@ class CurrentSessionViewController: UIViewController {
         return label
     }()
     
+    private lazy var timerLabel: UILabel = {
+        let label = UILabel()
+        label.text = "001"
+        label.font = .systemFont(ofSize: 30)
+        label.textColor = .black
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        addConstraints()
         view.backgroundColor = .green
     }
-
+    
+    private func addConstraints() {
+        
+        view.addSubview(wordsManagingButtonsStack)
+        view.addSubview(timerLabel)
+        view.addSubview(currentWordLabel)
+                        
+        wordsManagingButtonsStack.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            wordsManagingButtonsStack.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100),
+            wordsManagingButtonsStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            wordsManagingButtonsStack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50)
+        ])
+        
+        timerLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            timerLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            timerLabel.bottomAnchor.constraint(equalTo: wordsManagingButtonsStack.topAnchor, constant: -20)
+        ])
+        
+        currentWordLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            currentWordLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            currentWordLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    @objc private func startPausePressed() {
+        
+        if timer.isValid {
+            timer.invalidate()
+            return
+        }
+        
+        timer = Timer.scheduledTimer(
+            timeInterval: 1,
+            target: self,
+            selector: #selector(timeChanged),
+            userInfo: nil,
+            repeats: true
+        )
+    }
+    
+    @objc private func timeChanged() {
+        
+        time -= 1
+        
+        if time == 0 {
+            timer.invalidate()
+        }
+    }
 }
