df0b9b9dc588e68bd438d7acc3376c2f32f0c95d
niknyazev <n.knyazev.dev@gmail.com>
2022-08-22 18:02:12 +0300

    Add View model class for current session

---
+++ AliasLite/Controllers/CurrentSession/CurrentSessionViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  CurrentSessionViewModel.swift
+//  AliasLite
+//
+//  Created by Николай on 22.08.2022.
+//
+
+import Foundation
+
+protocol CurrentSessionViewModelProtocol {
+    var playerName: String { get }
+    var currentWord: String { get }
+    var wordsDropped: Int { get }
+    var wordsGuessed: Int { get }
+    func nextWord()
+}
+
+class CurrentSessionViewModel: CurrentSessionViewModelProtocol {
+    
+    var playerName: String = ""
+    var currentWord: String = ""
+    var wordsDropped: Int = 10
+    var wordsGuessed: Int = 10
+    
+    func nextWord() {
+        
+    }
+}
